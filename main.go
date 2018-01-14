47439975e8622e741484a9986f5bad6877c20d71
唐俊 <tangjun@tangjundeMacBook.local>
2018-01-14 12:16:03 +0800

    l10

--- main.go
+++ main.go
@@ -4,10 +4,15 @@ import "fmt"
 import "net/http"
 
 func Hello(response http.ResponseWriter, request *http.Request) {
-	fmt.Fprintf(response, "Hello, Welcome to go web programming...")
+	fmt.Fprintf(response, "hehheehhe...")
+}
+
+func Xixi(response http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(response, "xixiixixix...")
 }
 
 func main() {
 	http.HandleFunc("/kkk", Hello)
+	http.HandleFunc("/vvv", Xixi)
 	http.ListenAndServe(":8080", nil)
 }
